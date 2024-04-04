e3de3410b3ffc15b4821a4e86a16899fdf9c241f
seunghuicho <122choco@naver.com>
2024-04-04 22:36:34 +0900

    24.04.04 commit

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-alert('hi');
+// let a = 5;
+// const b = 2;
+// const myName = 'sh';
+
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log('hello '+myName);
+
+
+const player = {
+    name:'seunghui',
+    points: 10,
+    age: 26,
+    student: true,
+};
+console.log(player);
+console.log(player.name);
+console.log(player['name']);
+
+console.log(player);
+player.points = 20;
+console.log(player);
+
+player.home = 'Incheon';
+console.log(player);
